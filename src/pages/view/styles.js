26a13c79edaab06497c003c3dbd0f232199c83ab
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2022-10-27 20:14:55 -0500

    Avance al momento

--- src/pages/view/styles.js
+++ src/pages/view/styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { Container, Row } from "reactstrap";
 
-
 export const MainContainer = styled(Container)`
   background-image: url(${(props) => props.bgImg});
   height: 100%;
   background-position: center;
   background-size: cover;
 `;
+
 
 export const RowAfterHeader = styled(Row)`
 margin-top: 70px;
--- src/pages/view/view.js
+++ src/pages/view/view.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import {Row, Col } from "reactstrap";
 import { Content } from "../Content/content";
 import { Header } from "../../components/PageComponentes/Header";
 import Footer from "../../components/PageComponentes/Footer";
@@ -16,30 +16,30 @@ const navbaritems = [
 export const View = (props) => {
 
   return(
+    <MainContainer fluid bgImg = {pageColors[props.theme].bgImg}>
 
-    <MainContainer bgImg = {pageColors[props.theme].bgImg} fluid >
-      <Row>
-        {
-          props.header === 'Header' ? (
-            <Header theme={props.theme} items={navbaritems} logged ={props.logged}></Header>
-          ) : (
-            <div></div>
-          )
-        }
-      </Row>
-      <RowAfterHeader>
-        <Col className="col-12" >
+        <Row>
           {
-            props.banner === 'Prueba' ? (
-              <h3>Aquí iría algún elemento extra como un anuncio o algo así dependiendo en cuál página estemos</h3>
+            props.header === 'Header' ? (
+              <Header theme={props.theme} items={navbaritems} logged ={props.logged}></Header>
             ) : (
               <div></div>
             )
           }
-          <Content>{props.children}</Content>
-        </Col>
-      </RowAfterHeader>
-      <Footer></Footer>
+        </Row>
+        <RowAfterHeader>
+          <Col className="col-12" >
+            {
+              props.banner === 'Prueba' ? (
+                <h3>Aquí iría algún elemento extra como un anuncio o algo así dependiendo en cuál página estemos</h3>
+              ) : (
+                <div></div>
+              )
+            }
+            <Content>{props.children}</Content>
+          </Col>
+        </RowAfterHeader>
+        <Footer></Footer>
     </MainContainer>
   );
 };
